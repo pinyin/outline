0d15ab7b2d4612eec3b660d172b9ec6ec17ca867
pinyin <bob432091@gmail.com>
2018-07-15 14:06:18 +0800

    add compensate

---
+++ src/compensate.ts
@@ -0,0 +1,30 @@
+import {scale} from '../vendor/transformation-matrix/scale'
+import {transform} from '../vendor/transformation-matrix/transform'
+import {translate} from '../vendor/transformation-matrix/translate'
+import {centerOf} from './centerOf'
+import {decompose} from './decompose'
+import {Outline} from './Outline'
+import {Transform} from './Transform'
+
+/**
+ * Transform parent while keeping child's visual position
+ * @returns a transform to apply to child
+ */
+export function compensate(parent: Outline, parentDiff: Transform, child: Outline): Transform {
+    const outer = decompose(parentDiff)
+
+    const deltaX = centerOf(child).x - centerOf(parent).x
+    const deltaY = centerOf(child).y - centerOf(parent).y
+
+    return transform(
+        scale(1 / outer.scale.x, 1 / outer.scale.y),
+        translate(
+            -(deltaX * (outer.scale.x - 1)),
+            -(deltaY * (outer.scale.y - 1)),
+        ),
+        translate(
+            -outer.translate.x,
+            -outer.translate.y,
+        ),
+    )
+}
